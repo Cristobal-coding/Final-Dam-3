e0e338048df67f20717f3210db8f529bc00a6451
Cristobal-coding <agartkg95@gmail.com>
2021-08-08 02:42:40 -0400

    a

--- lib/pages/games.dart
+++ lib/pages/games.dart
@@ -1,3 +1,5 @@
+import 'package:final_dam_3/constants.dart';
+import 'package:final_dam_3/services/firestore.dart';
 import 'package:flutter/material.dart';
 
 class JuegosPage extends StatefulWidget {
@@ -8,10 +10,33 @@ class JuegosPage extends StatefulWidget {
 }
 
 class _JuegosPageState extends State<JuegosPage> {
+  FireStoreService juegos = FireStoreService();
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text('Juegos page')),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Juegos Disponibles',
+              style: TextStyle(
+                  fontFamily: titulosFontFamily,
+                  fontSize: 25,
+                  color: kSecondaryColor),
+            ),
+          ),
+          StreamBuilder(
+            stream: FireStoreService().juegos(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return Text('data');
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,1 +1,11 @@
-// Nothing yet
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FireStoreService {
+  Stream<QuerySnapshot> juegos() {
+    return FirebaseFirestore.instance.collection('juegos').snapshots();
+  }
+
+  Stream<QuerySnapshot> hardware() {
+    return FirebaseFirestore.instance.collection('hardware').snapshots();
+  }
+}
