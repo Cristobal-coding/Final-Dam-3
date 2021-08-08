802b9927e31d1f381c956aa7044f2ef86ed1c3ee
Cristobal-coding <agartkg95@gmail.com>
2021-08-08 03:30:00 -0400

    a

--- lib/pages/games.dart
+++ lib/pages/games.dart
@@ -1,3 +1,6 @@
+import 'dart:collection';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_dam_3/constants.dart';
 import 'package:final_dam_3/services/firestore.dart';
 import 'package:flutter/material.dart';
@@ -26,14 +29,44 @@ class _JuegosPageState extends State<JuegosPage> {
                   color: kSecondaryColor),
             ),
           ),
-          StreamBuilder(
-            stream: FireStoreService().juegos(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Center(child: CircularProgressIndicator());
-              }
-              return Text('data');
-            },
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(8.0),
+            height: 450,
+            child: StreamBuilder(
+              stream: FireStoreService().juegos(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                var juegos = snapshot.data.docs;
+                return ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  separatorBuilder: (_, __) => Divider(),
+                  itemCount: juegos.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.all(5.0),
+                      width: 250,
+                      height: double.infinity,
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20.0),
+                            child: Image.network(
+                              juegos[index]['img'],
+                              // height: 400,
+                              // width: 300,
+                            ),
+                          ),
+                          Text(juegos[index]['nombre'])
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
           ),
         ],
       ),
